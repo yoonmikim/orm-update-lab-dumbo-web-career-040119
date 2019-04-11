d680fe343a57758c8b039d202079556a7c248be4
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-04-11 02:24:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,7 +39,7 @@ class Student
         VALUES (?, ?)
       SQL
       DB[:conn].execute(sql, self.name, self.grade)
-      @id = DB[:conn].execute(sql, "SELECT FROM last_insert_rowid() FROM students")[0][0]
+      @id = DB[:conn].execute("SELECT FROM last_insert_rowid() FROM students")[0][0]
     end
   end 
   
