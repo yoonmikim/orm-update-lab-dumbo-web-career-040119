80f54faf370d01dd602784744dd6f27a78f5b3c8
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-04-11 03:18:41 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -29,4 +29,46 @@ class Student
     SQL
     DB[:conn].execute(sql)
   end
+  
+  def save 
+    if self.id 
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO students (name, grade)
+        VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end 
+  
+  def self.create(name, grade)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
+  
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    self.new(id, name, grade)
+  end
+  
+  def self.find_by_name(name) 
+    sql = <<-SQL
+      SELECT *
+      FROM students 
+      WHERE name = ?
+      LIMIT 1
+    SQL
+    DB[:conn].execute(sql,name).map{|row|self.new_from_db(row)}.first
+  end
+  
+  def update 
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    result = DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
+  
 end
