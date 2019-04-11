5c2d872169fb58c780510181770f0541652750ef
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-04-11 03:12:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -68,8 +68,7 @@ class Student
   
   def update 
     sql = "SELECT * FROM students SET name = ?, grade = ?, WHERE id = ?"
-    result = DB[:conn].execute(sql, name)[0]
-    Student.new(result[0], result[1], result[2])
+    result = DB[:conn].execute(sql, name)
   end
   
 end
