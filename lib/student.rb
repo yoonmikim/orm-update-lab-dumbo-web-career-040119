88c1806b4f279ce7d6cbb653a86d6fe25409a6c3
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-04-11 01:24:56 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,22 @@
 require_relative "../config/environment.rb"
 
 class Student
-
+  attr_accessor :name, :grade 
+  attr_reader :id
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
-
+  def initialize(id=nil, name, grade)
+    @id = id
+    @name = name 
+    @grade = grade
+  end
+  
+  def self.create_table 
+    sql = <<-SQL 
+      CREATE TABLE from students
+      
+    SQL
+  end
+  
 end
